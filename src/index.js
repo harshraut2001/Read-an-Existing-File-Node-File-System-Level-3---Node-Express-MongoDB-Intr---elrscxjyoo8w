2195bf391f4621e267f4ffa19eb145a061876913
harshraut2001 <137713248+harshraut2001@users.noreply.github.com>
2023-11-20 22:50:30 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,18 @@ const fs = require("fs/promises");
 const fileName = "myfile.txt";
 const reader = async (fileName) => {
   // write code here
-	// dont change function name
-  
-};
+	try{
+	const content = await fs.readfile(file);
+	console.log(content.toString())
+		return content.toString()
+	}catch(err){
+		console.log(err);
+	}
+    };
+	reader(fileName)
+	.then({content} => {
+		console.log("content",content)
+	})
 
 
 module.exports =  reader ;
